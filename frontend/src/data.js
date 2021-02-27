c42d6a6108483c55a86bc2e826d91cab37d4fee3
yangseung-eun <ksook12@gmail.com>
2021-02-27 12:43:27 +0900

    nodejs Server connect

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            brnad:'Nike',
+            rating:4.5,
+            numReviews: 10,
+            description:'high quility product'
+        },
+        {
+            _id:'2',
+            name: 'Adidas Fit Shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 100,
+            brnad:'Adidas',
+            rating:4.0,
+            numReviews: 10,
+            description:'high quility product'
+        },
+        {
+            _id:'3',
+            name: 'Lacost Free Shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 220,
+            brnad:'Lacost',
+            rating:4.8,
+            numReviews: 17,
+            description:'high quility product'
+        },
+        {
+            _id:'4',
+            name: 'Puma Slim Pant',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 78,
+            brnad:'Puma',
+            rating:4.5,
+            numReviews: 10,
+            description:'high quility product'
+        },
+        {
+            _id:'5',
+            name: 'Puma Slim Pant',
+            category: 'Pants',
+            image: '/images/p5.jpg',
+            price: 65,
+            brnad:'Puma',
+            rating:4.5,
+            numReviews: 10,
+            description:'high quility product'
+        },
+        {
+            _id:'6',
+            name: 'Adidas Fit Pant',
+            category: 'Pants',
+            image: '/images/p6.jpg',
+            price: 139,
+            brnad:'Adidas',
+            rating:4.5,
+            numReviews: 15,
+            description:'high quility product'
+        },
+       
+    ]
+}
+export default data;
